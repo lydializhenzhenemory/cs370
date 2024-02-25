ee40e5bc5a3f8f31db189bfc43485d68eab661ee
IrisZheng03 <143010945+IrisZheng03@users.noreply.github.com>
2024-02-25 17:05:28 -0500

    corrected story database with 52 stories

--- story_database/creating_db.py
+++ story_database/creating_db.py
@@ -22,3 +22,21 @@ with open('story_database/story_table.csv', 'r', encoding='utf-8') as csvfile:
 db_connection.commit()
 cursor.close()
 db_connection.close()
+
+
+''' #checking if there are three elements as expected
+import csv
+
+def clean_row(row):
+    return [item.replace('"', '').replace("'", "") for item in row]
+
+with open('story_database/story_table.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)  # Skip header row
+    for row_number, row in enumerate(csvreader, start=2):  # Start from 2 assuming first row is the header
+        if len(row) != 3:
+            print(f"Row {row_number} is malformed: {row}")
+        cleaned_row = clean_row(row)
+        print(f"Row {row_number} cleaned: {cleaned_row}")
+
+'''
