54e35c5d74f7aa163aaedc6a31cff723fc6b19fc
IrisZheng03 <143010945+IrisZheng03@users.noreply.github.com>
2024-03-04 21:13:44 -0500

    updated user table

--- story_database/story_table.sql
+++ story_database/story_table.sql
@@ -6,5 +6,22 @@ CREATE TABLE stories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     story_name VARCHAR(255),
     surface_story TEXT,
-    truth TEXT
+    truth TEXT,
+    average_attempts FLOAT DEFAULT 0
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE user_story_attempts (
+    user_id INT,
+    story_id INT,
+    question_attempts INT,
+    PRIMARY KEY (user_id, story_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (story_id) REFERENCES stories(id) ON DELETE CASCADE
 );
